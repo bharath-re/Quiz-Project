1633fdba590a27810304fb5f6cb9bd9bb40c49f7
Bharath <bharathb.w75@gmail.com>
2024-11-18 10:11:34 -0600

    final changes

---
+++ src/components/Answer.js
@@ -0,0 +1,14 @@
+function Answer() {
+  return (
+    <>
+      <div className="answer">
+        <div className="answer">Answer 1 </div>
+        <div className="answer">Answer 2</div>
+        <div className="answer">Answer 3</div>
+        <div className="answer"> Answer 4 </div>
+      </div>
+    </>
+  );
+}
+
+export default Answer;
